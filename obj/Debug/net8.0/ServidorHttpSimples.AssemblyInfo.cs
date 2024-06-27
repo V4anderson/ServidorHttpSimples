8ef467d891056ac3769e2fab4c4eed74538c8d85
Vanderson Andrade <vandersonandroid321@gmail.com>
2024-06-27 15:49:05 -0300

    Função para passar parametros na URL

--- obj/Debug/net8.0/ServidorHttpSimples.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServidorHttpSimples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3762957ffa29934ede86e8a8f861ab698e312166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da675566d395a52afc4193ef71bc0b51af976e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
